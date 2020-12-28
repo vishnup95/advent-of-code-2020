4002195e6b9dff73dcd016f929b0d1cf37b9d236
vishnup95 <vishnu.wish7@gmail.com>
2020-12-28 18:13:00 +0530

    Added Day 15 part2. Reddit helped..

--- Day 15/day15.js
+++ Day 15/day15.js
@@ -4,18 +4,29 @@ async function readInputData() {
   return text.split("\n"); // manipulating the input...
 }
 
-const playTheElfGame = (numbers) => {
-  while (numbers.length !== 2020) {
-    let lastSpokenNumber = numbers[numbers.length - 1];
-    let indexOfLastSpokenNumber = numbers.lastIndexOf(lastSpokenNumber, -2);
-    if (indexOfLastSpokenNumber === -1) numbers.push(0);
-    else numbers.push(numbers.length - (indexOfLastSpokenNumber + 1));
-    //numbers.push(1)
-  }
-  console.log(numbers)
-};
 
+// Thanks to reddit for knowledge about MAP. i love JS
+
+
+const play = (numbers, times = 2020) => {
+  //const numbers = ip.split(',').map(n => +n);
+  const memory = new Map();
+  numbers.forEach((n, i) => memory.set(n, i + 1));
+  let current = 0;
+  for (let turn = numbers.length + 1; turn < times; turn++) {
+    if (memory.has(current)) {
+      const lt = memory.get(current);
+      memory.set(current, turn);
+      current = turn - lt;
+    } else {
+      memory.set(current, turn);
+      current = 0;
+    }
+  }
+  console.log(current);
+};
 readInputData().then((data) => {
   const startingData = data[0].split(",").map((ele) => parseInt(ele, 10));
-  playTheElfGame(startingData);
+ // change to 2020 for part1
+  play(startingData, 30000000);
 });
