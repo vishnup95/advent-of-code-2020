1aa74f91d8b26cc4319560f6ad1a8d64da4ea8cf
vishnup95 <vishnu.wish7@gmail.com>
2021-01-03 16:19:06 +0530

    Added day 17 part2.(takes a long time)

--- Day 17/day17.js
+++ Day 17/day17.js
@@ -1,3 +1,4 @@
+console.time("conway")
 async function readInputData() {
   const res = await fetch("input.txt");
   const text = await res.text();
@@ -6,7 +7,7 @@ async function readInputData() {
 
 //
 conwayCalc = (cubesArr, cntr) => {
-  console.log(cubesArr);
+  // console.log(cubesArr);
   let phase = 1;
   while (phase !== 7) {
     let count = 0;
@@ -16,11 +17,10 @@ conwayCalc = (cubesArr, cntr) => {
         for (let x = 1; x < cubesArr[z][0].length - 1; x++) {
           let neighbourSpheres = rtnNeighbours(cubesArr, z, y, x);
           let activeSpheres = neighbourSpheres.filter((ele) => ele === "#");
-          let inActiveSpheres = neighbourSpheres.filter((ele) => ele === ".");
           if (cubesArr[z][y][x] === "#") {
             if (activeSpheres.length === 2 || activeSpheres.length === 3) {
               cubesArrCopy[z][y][x] = "#";
-              count++
+              count++;
             } else {
               cubesArrCopy[z][y][x] = ".";
             }
@@ -36,15 +36,64 @@ conwayCalc = (cubesArr, cntr) => {
     }
     cubesArr = cubesArrCopy;
     //console.log(cubesArr);
-    console.log(count);
+    console.log(`The number of active cubes in phase ${phase}`, count);
     console.log("========");
     phase++;
   }
   console.log(cubesArr);
+  console.log("========== PART 2 BELOW ========");
 };
 
+/*
+   Part2 calculations below. 
+*/
+
+conwayCalcPart2 = (cubesArr, cntr) => {
+  // console.log(cubesArr);
+  let phase = 1;
+  while (phase !== 7) {
+    let count = 0;
+    let cubesArrCopy = JSON.parse(JSON.stringify(cubesArr));
+    for (let w = cntr - phase; w <= cntr + phase; w++) {
+      for (let z = 1; z < cubesArr[w].length - 1; z++) {
+        for (let y = 1; y < cubesArr[z].length - 1; y++) {
+          for (let x = 1; x < cubesArr[z][0].length - 1; x++) {
+            let neighbourSpheres = testNeighbours(cubesArr, w, z, y, x);
+            let activeSpheres = neighbourSpheres.filter((ele) => ele === "#");
+            if (cubesArr[w][z][y][x] === "#") {
+              if (activeSpheres.length === 2 || activeSpheres.length === 3) {
+                cubesArrCopy[w][z][y][x] = "#";
+                count++;
+              } else {
+                cubesArrCopy[w][z][y][x] = ".";
+              }
+            }
+            if (cubesArr[w][z][y][x] === ".") {
+              if (activeSpheres.length === 3) {
+                cubesArrCopy[w][z][y][x] = "#";
+                count++;
+              }
+            }
+          }
+        }
+      }
+    }
+    cubesArr = cubesArrCopy;
+    console.log(`The number of active cubes in phase ${phase}`, count);
+    console.log("========");
+    phase++;
+  }
+  console.log(cubesArr);
+  console.timeEnd("conway")
+};
+
+/*
+  General  
+*/
 readInputData().then((data) => {
   let zArr = [];
+  let wArr = [];
+  let zArrTemp = [];
   const xTemp = new Array(data.length * 3).fill(".");
   data = data.map((ele) => {
     return [...xTemp, ...ele, ...xTemp];
@@ -66,8 +115,22 @@ readInputData().then((data) => {
     }
     zArr.push(zTemp);
   }
-  //console.log("yTemp Length", yTemp.length);
+
   conwayCalc(zArr, Math.floor(yTemp.length / 2));
+
+  // Part 2 from here....
+
+  for (let index = 0; index <= yTemp.length - 1; index++) {
+    zArrTemp.push(zTemp);
+  }
+
+  for (let index = 0; index < zArr.length - 1; index++) {
+    if (index === Math.floor(zArr.length / 2)) {
+      wArr.push(zArr);
+    }
+    wArr.push(zArrTemp);
+  }
+  conwayCalcPart2(wArr, Math.floor(wArr.length / 2));
 });
 
 function rtnNeighbours(cubes, zPos, yPos, xPos) {
@@ -84,6 +147,25 @@ function rtnNeighbours(cubes, zPos, yPos, xPos) {
       }
     }
   }
-
   return neighbours;
+}
+
+function testNeighbours(cubes, wPos, zPos, yPos, xPos) {
+  let neighbours = [];
+  for (let w = -1; w <= 1; w++) {
+    for (let z = -1; z <= 1; z++) {
+      for (let y = -1; y <= 1; y++) {
+        for (let x = -1; x <= 1; x++) {
+          if (z === 0 && y === 0 && x === 0 && w === 0) {
+            //console.log("Not needed", z, y, x);
+          } else {
+            //console.log(z, y, x);
+            neighbours.push(cubes[wPos + w][zPos + z][yPos + y][xPos + x]);
+          }
+        }
+      }
+    }
+  }
+  return neighbours;
+  //console.log(neighbours);
 }
